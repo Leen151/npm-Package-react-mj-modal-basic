597ac1852141139a1bea15e55da9b4ed15d7d3f5
Leen151 <leen.m@orange.fr>
2025-05-11 09:00:21 +0200

    fichiers de configuration du package

---
+++ vite.config.js
@@ -0,0 +1,24 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
+
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/index.js'),
+      name: 'ReactMjModalBasic',
+      fileName: (format) => `react-mj-modal-basic.${format}.js`
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom', 'prop-types'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+          'prop-types': 'PropTypes'
+        }
+      }
+    }
+  }
+})
